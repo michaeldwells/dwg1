cf4af186a6a024170b955fa4672c63f2dd5fdba3
michaeldwells <michael.d.wells@gmail.com>
2023-12-17 17:26:12 +0000

    Added new game DWG4
    x.js now supports pseudorandom stuff.

--- x.js
+++ x.js
@@ -19,6 +19,70 @@ function shuffle(a) {
 		a[index] = temp;
 	}
 }
+
+function cyrb128(str) { // 128-bit hash.
+    let h1 = 1779033703, h2 = 3144134277,
+        h3 = 1013904242, h4 = 2773480762;
+    for (let i = 0, k; i < str.length; i++) {
+        k = str.charCodeAt(i);
+        h1 = h2 ^ Math.imul(h1 ^ k, 597399067);
+        h2 = h3 ^ Math.imul(h2 ^ k, 2869860233);
+        h3 = h4 ^ Math.imul(h3 ^ k, 951274213);
+        h4 = h1 ^ Math.imul(h4 ^ k, 2716044179);
+    }
+    h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067);
+    h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233);
+    h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213);
+    h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179);
+    h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1;
+    return [h1>>>0, h2>>>0, h3>>>0, h4>>>0];
+}
+function sfc32(a, b, c, d) { // Pseudo-random number generator with 128-bit seed.
+    return function() {
+      a >>>= 0; b >>>= 0; c >>>= 0; d >>>= 0; 
+      var t = (a + b) | 0;
+      a = b ^ b >>> 9;
+      b = c + (c << 3) | 0;
+      c = (c << 21 | c >>> 11);
+      d = d + 1 | 0;
+      t = t + d | 0;
+      c = c + t | 0;
+      return (t >>> 0) / 4294967296;
+    }
+}
+function mulberry32(a) { // Pseudo-random number generator with 32-bit seed.
+    return function() {
+      var t = a += 0x6D2B79F5;
+      t = Math.imul(t ^ t >>> 15, t | 1);
+      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+function prgn_generator(a) {return mulberry32(a)}
+function hash(s) {return cyrb128(s)[0]}
+var prng;
+var prng_seed;
+function seedPrng(s = hash(Date.now().toString())) {
+	prng_seed = s;
+	prng = prgn_generator(prng_seed);
+}
+seedPrng();
+function prnd(max) {return M.floor(prng() * M.floor(max));}
+function prndw(max, weight) {return M.floor((prng() ** weight) * M.floor(max));}
+function popt(a) {return a.at(prnd(a.length));}
+function ppick(a) {return a.splice(prnd(a.length),1)[0];}
+function ppickw(a, weight) {return a.splice(prndw(a.length, weight),1)[0];}
+function pshuffle(a) {
+	let counter = a.length;
+	while (counter > 0) {
+		let index = Math.floor(prng() * counter);
+		counter--;
+		let temp = a[counter];
+		a[counter] = a[index];
+		a[index] = temp;
+	}
+}
+
 function permutations(l) {
 	if (l.length == 1) return [l];
 	if (l.length == 0) return [];
